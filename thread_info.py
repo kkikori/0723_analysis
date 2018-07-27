91f298e111c8389bd22471fbfb37ba6e97cc2b80
ida <poohmaki0506@gmail.com>
2018-07-27 18:33:05 +0900

    余分なものを消した

--- thread_info.py
+++ thread_info.py
@@ -30,7 +30,7 @@ def _comment_nums(Thread_list, Post_list, group_n):
     names_label = []
     coms = []
     for th_i, thread in Thread_list.items():
-        names_label.append(thread.title)
+        names_label.append(thread.id)
         pi_except_other_list = _except_others(thread.pi_list, Post_list)
         coms.append(len(pi_except_other_list))
 
@@ -38,7 +38,7 @@ def _comment_nums(Thread_list, Post_list, group_n):
     plt.bar(list(range(len(coms))), coms, tick_label=names_label, align="center")
     plt.title(group_n)
     plt.ylim(0, 70)
-    plt.xticks(list(range(len(coms))), names_label, rotation=70)
+    plt.xticks(list(range(len(coms))), names_label)
     plt.savefig(fn_path + group_n + "_comment_num" + ".png")
     plt.show()
 
@@ -57,77 +57,6 @@ def _comment_nums(Thread_list, Post_list, group_n):
     print("平均,", sum(coms) / len(coms))
 
 
-def _temporal_sequence_extract(thread, Post_list):
-    # startからfilishまで5分刻みで投稿の個数を数える
-    nums = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], \
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-
-    for pi in thread.pi_list:
-
-        num_i = 0
-        while num_i < len(nums):
-            limit_t = start_t + five_min * (num_i + 1)
-            if Post_list[pi].created_at < limit_t:
-                if Post_list[pi].user_id in except_usr_id:
-                    nums[num_i][1] += 1
-                else:
-                    nums[num_i][0] += 1
-                break
-            num_i += 1
-
-    nums[11][0] += nums[12][0]
-    nums[11][1] += nums[12][1]
-    nums.pop(-1)
-
-    return np.array(nums)
-
-
-def _check_start(counts):
-    height = list(counts[:, 0])
-    print(height)
-
-    for i, post_num in enumerate(height):
-        if post_num == 0:
-            continue
-        r_height = height[i:]
-        print("r_height", r_height)
-        r_left = list(range(i, len(height)))
-        print("r_left  ", r_left)
-        if len(r_height) != len(r_left):
-            print("you cannot list")
-            return False
-        return r_left, r_height
-    return None, None
-
-
-def _time_series(Thread_list, Post_list, group_n):
-    # 抽出
-    thread_post_nums = []
-    titles = []
-    for th_i in range(1, len(Thread_list)):
-        thread = Thread_list[th_i]
-        print(" title", thread.title)
-        thread_post_nums.append(_temporal_sequence_extract(thread, Post_list))
-        titles.append(thread.title)
-
-    # とりあえず表示
-    # 折れ線グラフを出力
-
-    for th_i, counts in enumerate(thread_post_nums):
-        thread = Thread_list[th_i + 1]
-
-        left, height = _check_start(counts)
-        if left == None:
-            continue
-        plt.plot(left, height, label=thread.id)
-        plt.plot(left[0], height[0], 'o')
-
-    plt.xticks(list(range(len(thread_post_nums))),
-               ["2018/07/23 \n 16:20", "", "16:30", "", "16:40", "", "16:50", "", "17:00", "", "17:10", "", "17:20"], )
-    plt.legend()
-    plt.show()
-
-
 def thread_analysis_main(Thread_list, Post_list, group_n):
-    # _comment_nums(Thread_list, Post_list, group_n)
-    _time_series(Thread_list, Post_list, group_n)
+    _comment_nums(Thread_list, Post_list, group_n)
+    #_time_series(Thread_list, Post_list, group_n)
